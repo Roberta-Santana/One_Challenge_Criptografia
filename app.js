02b74b0a2fb98d2e73c60de655e2e23918e3223b
Roberta Santana <g.roberta.s@hotmail.com>
2024-02-22 21:45:03 -0300

    tentanto fazer a div da saída de texte

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-let texto = document.querySelector('');
-texto.innerHTML
+/*1- AO CLICK DO BOTÃO Pegar o texto digitado na caixa,
+2- converter os elementos do texto (lista ?? de string?)
+3-carregar texto convertido na caixa de texto descrip*/
+
+let textoInicial;
+
+function criptografarButton(){
+    textoInicial = document.querySelector('input').value;
+
+}
+
+function exibiTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial(){
+    exibiTextoNaTela('h2','Nenhuma mensagem encontrada');
+    exibiTextoNaTela('p',`${texto_criptografado}`);
+}
