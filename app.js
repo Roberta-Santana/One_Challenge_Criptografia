b0ef3c2116e58d3c89693f6b90b819174b15da2e
Roberta Santana <g.roberta.s@hotmail.com>
2024-02-26 20:39:35 -0300

    quase lá faltando ajustar as repetições

--- app.js
+++ app.js
@@ -3,10 +3,18 @@
 3-carregar texto convertido na caixa de texto descrip*/
 
 let textoInicial;
+let novoTexto;
 
 function exibiTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+}
+
+function substituirTexto(letraA, letraB){
+    if(textoInicial.includes(letraA)){
+        textoInicial=textoInicial.replace(letraA,letraB)
+    }
+    return textoInicial;
 }
 
 function limparCxEntrada(){
@@ -15,10 +23,32 @@ function limparCxEntrada(){
 }
 function criptografarButton(){
     textoInicial = document.querySelector('input').value;
-        if(textoInicial.includes('a')){
-            textoInicial.replace('a', 'ai')}
-    
+        substituirTexto('e', 'enter');
+        console.log(textoInicial);
+        substituirTexto('i', 'imes');
+        console.log(textoInicial);
+        substituirTexto('o', 'ober');
+        console.log(textoInicial);
+        substituirTexto('u', 'ufat');
+        console.log(textoInicial); 
+        substituirTexto('a','ai');
+        console.log(textoInicial);
+
     limparCxEntrada();
-    return exibiTextoNaTela('#saida__caixa__texto',`${textoInicial}`);
-    
+    return exibiTextoNaTela('#saida__caixa__texto',`${textoInicial}`);    
+}
+
+function descriptografarButton(){
+    textoInicial = document.querySelector('input').value;   
+    substituirTexto('enter', 'e');
+    console.log(textoInicial);
+    substituirTexto('imes','i');
+    console.log(textoInicial);
+    substituirTexto('ai','a');
+    console.log(textoInicial);
+    substituirTexto('ober','o');
+    console.log(textoInicial);
+    substituirTexto('ufat','u');
+    console.log(textoInicial); 
+    return exibiTextoNaTela('#saida__caixa__texto',`${textoInicial}`); 
 }
