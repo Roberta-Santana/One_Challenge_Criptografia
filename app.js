1be43ce48637e4442b770b036d95221c29af3a7d
Roberta Santana <g.roberta.s@hotmail.com>
2024-03-02 19:53:23 -0300

    funcionando crip

--- app.js
+++ app.js
@@ -8,27 +8,21 @@ function exibiTextoNaTela(tag, texto){
 
 function limparTexto(tag){
     let limparCampo = document.querySelector(tag);
-    limparCampo.value = '';
+    return limparCampo.value = '';
 }
 
 function criptografarButton(){
     textoInicial = document.querySelector('input').value;
     textoSaida= textoInicial.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai")
                             .replace(/o/g, "ober").replace(/u/g, "ufat");
+    limparTexto('input');
     return exibiTextoNaTela('#saida__caixa__texto',`${textoSaida}`);
 }
 
-/* function descriptografarButton(){
-    textoInicial = document.querySelector('input').value;   
-    substituirTexto('enter', 'e');
-    console.log(textoInicial);
-    substituirTexto('imes','i');
-    console.log(textoInicial);
-    substituirTexto('ai','a');
-    console.log(textoInicial);
-    substituirTexto('ober','o');
-    console.log(textoInicial);
-    substituirTexto('ufat','u');
-    console.log(textoInicial); 
-    return exibiTextoNaTela('#saida__caixa__texto',`${textoInicial}`); 
-} */
+function descriptografarButton(){
+    var textoFinal = document.querySelectorAll('#saida__caixa__texto');
+    var textoDescrip= textoFinal.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a")
+                            .replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    return exibiTextoNaTela('#crip__texto',`${textoDescrip}`);
+}
