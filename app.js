f461a76dac226564af229b9dd33620d49f5e6431
Roberta Santana <g.roberta.s@hotmail.com>
2024-03-15 10:41:51 -0300

    ajustando css

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function criptografarButton(){
     textoInicial = document.querySelector('input').value;
     textoSaida= textoInicial.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai")
                             .replace(/o/g, "ober").replace(/u/g, "ufat");
-    limparTexto('input');
+    /*limparTexto('input');*/
     return exibiTextoNaTela('.saida__caixa',`${textoSaida}`);
 }
 
@@ -23,7 +23,6 @@ function descriptografarButton(){
    var textoFinal = textoSaida;
    var textoDescrip = textoFinal.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a")
                                 .replace(/ober/g, "o").replace(/ufat/g, "u");
-    limparTexto('.saida__caixa');
 /* LIMPAR TEXTO CRIPTOGRAFADO exibiTextoNaTela('input[type="text"]')*/
     return document.querySelector('input[type="text"]').value = textoDescrip; 
 }
